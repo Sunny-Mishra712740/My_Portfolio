fbfee24f5f65ba2e6d89873ff64565eeaac0bd26
Sunny Mishra <sunnycoding712740@gmail.com>
2025-10-29 19:31:40 +0530

    Starting the server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen("8080", () => {
+    console.log("Server is listening to the port: 8080")
+})
