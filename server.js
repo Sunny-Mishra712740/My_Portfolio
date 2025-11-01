79e5ccace07d93a349e0a5e49a909858ebb5d95a
Sunny Mishra <sunnycoding712740@gmail.com>
2025-11-01 23:28:53 +0530

    Day 01

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+// mongoose.connect("url")
+// const db = mongoose.connection
+// db.on("error", () => {
+//     console.log("Error while connecting to the mongoDB")
+// })
+// db.once("open", () => {
+//     console.log("Connected to MongoDB")
+// })
 
 
-
-app.listen("8080", () => {
-    console.log("Server is listening to the port: 8080")
+const PORT = process.env.PORT || 7777
+console.log(process.env.PORT)
+app.listen(PORT, () => {
+    console.log(`Server is listening to the port num : ${PORT}`)
 })
